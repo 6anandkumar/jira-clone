9f7ea62b6e2171bbab8e0508dd786ea9ed901b19
6anandkumar <61247305+6anandkumar@users.noreply.github.com>
2025-04-04 00:29:50 +0530

    Anand Commit

---
+++ backend/authentication-service/src/main/java/com/taskmanager/authentication/App.java
@@ -0,0 +1,16 @@
+package com.taskmanager.authentication;
+
+import com.taskmanager.common.TestCommonLib;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        TestCommonLib testCommonLib = new TestCommonLib();
+        System.out.println( "Hello World!" );
+    }
+}
